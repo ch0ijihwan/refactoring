5b4975f67f3a6d3d6fbe2de7c28566bed505bada
cjh8746 <cjh87467@gmail.com>
2022-10-17 16:23:54 +0900

    feat : 사용자 입력에 따른 DisplayMode 를 나타낼 수 있 DisplayMode 구현

--- src/refactor/model/domain/calculator/Subtraction.java
+++
@@ -1,9 +0,0 @@
-package refactor.model.domain.calculator;
-
-public class Subtraction implements Calculation {
-
-    @Override
-    public Integer calculate(int x, int y) {
-        return x - y;
-    }
-}
