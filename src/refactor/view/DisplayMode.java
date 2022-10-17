03eb5bc276710450909ca72d49676181d224bd22
cjh8746 <cjh87467@gmail.com>
2022-10-17 16:23:54 +0900

    feat : 사용자 입력에 따른 DisplayMode 를 나타낼 수 있 DisplayMode 구현

---
+++ src/refactor/view/DisplayMode.java
@@ -0,0 +1,26 @@
+package refactor.view;
+
+import java.util.Arrays;
+
+public enum DisplayMode {
+    
+    WON_TO_DECIMAL(0),
+    WON_TO_BINARY(1),
+    WON_TO_HEX(2),
+    WON_TO_DOLLAR_TO_DECIMAL(3),
+    WON_TO_DOLLAR_TO_BINARY(4),
+    WON_TO_DOLLAR_TO_HEX(5);
+
+    private final int value;
+
+    DisplayMode(int value) {
+        this.value = value;
+    }
+
+    public static DisplayMode valueOf(int modeNumber) {
+        return Arrays.stream(DisplayMode.values())
+                .filter(mode -> mode.value == modeNumber)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 모드입니다."));
+    }
+}
