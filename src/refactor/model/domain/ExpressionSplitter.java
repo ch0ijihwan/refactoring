f50c08df0f604309e2246b3cce948026cde62381
cjh8746 <cjh87467@gmail.com>
2022-10-17 16:42:45 +0900

    refactor : 매직넘버를 기호 상수로 변경 및 매서드 파라미터를 final 로 받도록 변경

--- src/refactor/model/domain/ExpressionSplitter.java
+++ src/refactor/model/domain/ExpressionSplitter.java
@@ -9,46 +9,48 @@ import java.util.stream.Collectors;
 
 public class ExpressionSplitter {
 
+    private static final int FIRST_NUMBER_INDEX = 0;
+    private static final int SECOND_NUMBER_INDEX = 1;
+    private static final String BLANK = " ";
+    private static final String DELIMITER = "";
 
     private final List<Integer> operands;
     private final Symbol symbol;
 
-    public ExpressionSplitter(String expression) {
+    public ExpressionSplitter(final String expression) {
         String noneBlankExpression = removeBlanks(expression);
         symbol = findSymbol(noneBlankExpression);
         operands = findNumbers(noneBlankExpression, symbol);
     }
 
-    private String removeBlanks(String expression) {
-
-        List<String> tokens = Arrays.stream(expression.split(""))
-                .filter(value -> !value.equals(" "))
+    private String removeBlanks(final String expression) {
+        List<String> tokens = Arrays.stream(expression.split(DELIMITER))
+                .filter(value -> !value.equals(BLANK))
                 .collect(Collectors.toUnmodifiableList());
-        return String.join("", tokens);
+        return String.join(DELIMITER, tokens);
     }
 
-
-    private Symbol findSymbol(String expression) {
+    private Symbol findSymbol(final String expression) {
         return Arrays.stream(Symbol.values())
                 .filter(targetSymbol -> expression.contains(targetSymbol.getValue()))
                 .findAny()
                 .orElseThrow();
     }
 
-    private List<Integer> findNumbers(String expression, Symbol symbol) {
+    private List<Integer> findNumbers(final String expression, final Symbol symbol) {
         List<Integer> numbers = new ArrayList<>();
         int symbolIndex = expression.indexOf(symbol.getValue());
         numbers.add(Integer.parseInt(expression.substring(0, symbolIndex)));
-        numbers.add(Integer.parseInt(expression.substring(symbolIndex)));
+        numbers.add(Integer.parseInt(expression.substring(symbolIndex + 1)));
         return numbers;
     }
 
     public Integer getFirstNumber() {
-        return operands.get(0);
+        return operands.get(FIRST_NUMBER_INDEX);
     }
 
     public Integer getSecondNumber() {
-        return operands.get(1);
+        return operands.get(SECOND_NUMBER_INDEX);
     }
 
     public Symbol getSymbol() {
