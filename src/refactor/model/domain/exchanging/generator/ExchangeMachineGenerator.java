50947a64e126b9f6abd196dbcb57889f8ab241bc
cjh8746 <cjh87467@gmail.com>
2022-10-17 16:20:22 +0900

    feat : MoneyExchangingMachine 팩토리 구현

---
+++ src/refactor/model/domain/exchanging/generator/ExchangeMachineGenerator.java
@@ -0,0 +1,25 @@
+package refactor.model.domain.exchanging.generator;
+
+import refactor.model.domain.exchanging.ExchangeToDollar;
+import refactor.model.domain.exchanging.ExchangeToWon;
+import refactor.model.domain.exchanging.MoneyExchangingMachine;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class ExchangeMachineGenerator {
+
+    private static final Map<ExchangeType, MoneyExchangingMachine> EXCHANGING_MACHINE_MAP = new EnumMap<>(ExchangeType.class);
+
+    static {
+        EXCHANGING_MACHINE_MAP.put(ExchangeType.TO_WON, new ExchangeToWon());
+        EXCHANGING_MACHINE_MAP.put(ExchangeType.TO_DOLLAR, new ExchangeToDollar());
+    }
+
+    public static MoneyExchangingMachine getMoneyExchangingMachineBy(ExchangeType exchangeType) {
+        return EXCHANGING_MACHINE_MAP.get(exchangeType);
+    }
+
+    private ExchangeMachineGenerator() {
+    }
+}
