3c8869cf31938e810150397342ddc1813a82a6f8
cjh8746 <cjh87467@gmail.com>
2022-10-17 16:25:18 +0900

    feat : 프로그램 실행 결과 출력을 위한 Output 구현

---
+++ src/refactor/view/output/ConsoleOutput.java
@@ -0,0 +1,19 @@
+package refactor.view.output;
+
+import refactor.view.DisplayMode;
+
+public class ConsoleOutput implements Output {
+
+    @Override
+    public void display(DisplayMode displayMode, Integer result) {
+        if (displayMode == DisplayMode.WON_TO_DECIMAL || displayMode == DisplayMode.WON_TO_DOLLAR_TO_DECIMAL) {
+            System.out.println(result);
+        }
+        if (displayMode == DisplayMode.WON_TO_BINARY || displayMode == DisplayMode.WON_TO_DOLLAR_TO_BINARY) {
+            System.out.println(Integer.toBinaryString(result));
+        }
+        if (displayMode == DisplayMode.WON_TO_HEX || displayMode == DisplayMode.WON_TO_DOLLAR_TO_HEX) {
+            System.out.println(Integer.toHexString(result));
+        }
+    }
+}
