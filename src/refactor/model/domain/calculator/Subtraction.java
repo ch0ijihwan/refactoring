0f43950eedcad5f3c4f62c68b49a55d01795ad4b
최지환 <cjh87467@gmail.com>
2022-10-17 20:51:38 +0900

    Delete src/refactor directory

--- src/refactor/model/domain/calculator/Subtraction.java
+++
@@ -1,9 +0,0 @@
-package refactor.model.domain.calculator;
-
-public class Subtraction implements Calculation {
-
-    @Override
-    public Integer calculate(final int x, final int y) {
-        return x - y;
-    }
-}
